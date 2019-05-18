8f737bed3ea6df97ba5c3b6b9509140bfcec4ca8
Jacob Tan <zhangen69@live.co.uk>
2019-05-18 11:27:09 +0800

    update

--- app.js
+++ app.js
@@ -1,33 +1,59 @@
+// ES6: import libraries
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import bodyParser from 'body-parser';
 
+// initialize configs
 const app = express();
 const port = 3000;
+const routes = express.Router();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
+// initialize functions
+const getHelloWorld = (req, res, next) => res.send('GET:Hello World!!!');
+const postHelloWorld = (req, res, next) => {
+  console.log(req.body);
+  res.send('POST:Hello World!!!');
+};
 const checkAuth = (req, res, next) => {
-    try {
-        if (!req.headers.authorization.split(' ')[1]) {
-            throw new Error('Access Denied!');
-        }
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Access Denied!' });
+  try {
+    if (!req.headers.authorization.split(' ')[1]) {
+      throw new Error('Access Denied!');
     }
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Access Denied!' });
+  }
 };
 
-// app.use('/', checkAuth);
+// setup routes
+routes.get('/hello1', getHelloWorld);
+routes.post('/hello2', postHelloWorld);
+routes.get('/hello3', checkAuth, getHelloWorld);
+
+// enables middlewares
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    // res.send('Hello World!');
-    console.log(req.query.query.toString());
-    res.json({ message: 'Hello World!' });
+// import routes into specific parent path
+app.use('/api', routes);
+app.get('/hello', function(req, res) {
+  res.render('demo', { title: 'Hey', message: 'Hello there!', imgUrl: './images/image1.jpg', paragaph: 'testing paragaph' });
 });
 
+// express static files
+// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'views')));
 app.use('/images', express.static(path.join(__dirname, 'assets')));
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, './meal-prep/dist', 'index.html'));
-});
+// app.use((req, res, next) =>
+//   res.sendFile(path.join(__dirname, './meal-prep/dist/index.html'))
+// );
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// run node express with listen port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+  console.log(`Start your application: http://localhost:${port}`);
+});
